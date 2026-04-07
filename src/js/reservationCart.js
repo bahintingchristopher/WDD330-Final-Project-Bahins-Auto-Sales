53be755b18d140462b7e1bfd83ea2d75ea525c31
Christopher Bahinting <bahintingchristopher@gmail.com>
2026-04-07 16:28:04 +0800

    modify the href of the finalization reserve.

--- src/js/reservationCart.js
+++ src/js/reservationCart.js
@@ -22,7 +22,7 @@ async function init() {
                 localStorage.removeItem('car-reservation-cart');
             
                 // window.location.href = "../thankyou/thankyou.html"; this was the original path, but it caused issues when deployed on render, so I changed it to an absolute path
-                window.location.href = "/thankyou/thankyou.html";
+                window.location.href = `${cleanBase}thankyou/thankyou.html`;
             
         });
     }
